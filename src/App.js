119c6142dc25b9a1deae3751253f3a31b05cef96
adarsh <adarshradhakrishna@bitcot.com>
2023-10-07 10:31:22 +0530

    added movement of a particle

--- src/App.js
+++ src/App.js
@@ -12,9 +12,18 @@ function App() {
 
     if(renderRefernce.current){
       const ctx=canvasRef.current.getContext('2d')
-      const effect= new Effect(canvasRef.current.height,canvasRef.current.width)
-      const p1= new Particle(0,0,50,ctx,effect)
-      p1.draw() 
+      const effect= new Effect(canvasRef.current.height,canvasRef.current.width,ctx)
+      effect.init()
+      
+
+
+      function animate(){
+      ctx.clearRect(0,0,canvasRef.current.width,canvasRef.current.height)
+      effect.draw()
+      effect.update()
+        requestAnimationFrame(animate)
+      }
+      animate()
     }
     
     renderRefernce.current=true
@@ -27,18 +36,38 @@ function App() {
       this.y= Math.random() * effect.height
       this.size=size
       this.ctx=ctx
+      this.vx=1
+      this.vy=1
      }
 
      draw(){
-      debugger;
     this.ctx.fillRect(this.x,this.y,this.size,this.size)
+     }
+
+     update(){
+      this.x+= this.vx
+      this.y+= this.vy
      }
   }
   class Effect{
-     constructor(height,width){
+     constructor(height,width,ctx){
       this.height=height
       this.width=width
-      
+      this.particleArray=[]
+      this.ctx=ctx
+     }
+     init(){
+      for(let i=0; i<=10;i++){
+        this.particleArray.push(new Particle(0,0,50,this.ctx,this))
+      }
+     }
+
+     draw(){
+       this.particleArray.forEach((particle)=> particle.draw())
+     }
+
+     update(){
+      this.particleArray.forEach((particle)=> particle.update())
      }
   }
   
@@ -60,3 +89,4 @@ export default App;
 
 // first we need to define 2 classes for 
 // 1 .Partcle and 2 for drawing All Paeticles
+// we need to move the particles 
